111f50afef661394ca49614ee5b4809e768ce1a3
VisualClony <mrcoolmoder@gmail.com>
2025-05-30 10:04:23 +0300

    Something changed

--- src/Repositories/Repositories/ICompetitionLevelRepository.cs
+++ src/Repositories/Repositories/ICompetitionLevelRepository.cs
@@ -9,21 +9,11 @@ namespace CompetitiveBackend.Repositories
 {
     public interface ICompetitionLevelRepository
     {
-        /// <summary>
-        /// Получить данные об уровне соревнования
-        /// </summary>
-        /// <param name="competitionID">Идентификатор соревнования</param>
-        /// <returns>Данные</returns>
-        /// <exception cref="CompetitiveBackend.Repositories.Exceptions.MissingDataException">Соревнование с таким ID не найдено</exception>
+       
         public Task<LargeData> GetCompetitionLevel(int competitionID, string? Platform = null, int? MaxVersion = null);
         public Task<LargeData> GetSpecificCompetitionLevel(int levelDataID);
         public Task<LevelDataInfo> GetSpecificCompetitionLevelInfo(int levelDataID);
-        /// <summary>
-        /// Загрузить уровень соревнования
-        /// </summary>
-        /// <param name="competitionID">Идентификатор соревнования</param>
-        /// <param name="levelData">Данные об уровне соревнования</param>
-        /// <exception cref="CompetitiveBackend.Repositories.Exceptions.MissingDataException">Соревнование с таким ID не найдено</exception>
+      
         public Task<IEnumerable<LevelDataInfo>> GetAllLevelData(int competitionID);
 
         public Task AddCompetitionLevel(LargeData data, LevelDataInfo levelData);
--- src/Services/Services/ICompetitionLevelService.cs
+++ src/Services/Services/ICompetitionLevelService.cs
@@ -14,11 +14,6 @@ namespace CompetitiveBackend.Services
         public Task UpdateCompetitionLevelData(int levelDataID, LargeData largeData);
         public Task UpdateCompetitionLevelInfo(LevelDataInfo levelDataInfo);
         public Task DeleteLevelData(int levelData);
-        /// <summary>
-        /// Получить данные об игровом представлении уровня соревнования
-        /// </summary>
-        /// <param name="competitionID">Идентификатор соревнования</param>
-        /// <returns>Данные об игровом представлении уровня соревнования</returns>
         public Task<LargeData> GetCompetitionLevel(int competitionID, string? platform = null, int? maxVersion = null);
         public Task<LargeData> GetSpecificCompetitionLevel(int levelDataID);
         public Task<LevelDataInfo> GetSpecificCompetitionLevelInfo(int levelDataID);
