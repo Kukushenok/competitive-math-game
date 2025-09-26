d71718fec77072502a6521efa6422c0169e609f2
VisualClony <mrcoolmoder@gmail.com>
2025-09-26 19:57:04 +0300

    Added more tests (a little)

--- src/IntegrationalTests/IntegrationalTests/AuthIntegrationTests.cs
+++ src/IntegrationalTests/IntegrationalTests/AuthIntegrationTests.cs
@@ -1,5 +1,9 @@
-﻿using CompetitiveBackend.BackendUsage.Objects;
+﻿using Allure.Net.Commons;
+using Allure.Xunit.Attributes.Steps;
+using Bogus;
+using CompetitiveBackend.BackendUsage.Objects;
 using FluentAssertions;
+using RepositoriesRealisation;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -10,6 +14,39 @@ using System.Threading.Tasks;
 
 namespace IntegrationalTests
 {
+    public class SetupAuth(HttpClient Client, BaseDbContext Context, string name) : IAsyncDisposable
+    {
+        private const string USER_PASSWORD = "123456789A123456789";
+        private bool registered = false;
+        public async ValueTask DisposeAsync()
+        {
+            if (!registered) return;
+            await AllureApi.Step("Remove account", async () =>
+            {
+                var acc = Context.AccountsReadOnly.Where(x => x.Login == name).ToList();
+                Context.AccountsReadOnly.RemoveRange(acc);
+                await Context.SaveChangesAsync();
+                registered = false;
+            });
+        }
+
+        public async Task<AuthSuccessResultDTO> RegisterAccount()
+        {
+            return await AllureApi.Step("Register account", async () =>
+            {
+                var regResult = await Client.PostAsJsonAsync("/api/v1/auth/register", new AccountCreationDTO(name, USER_PASSWORD, null));
+                regResult.IsSuccessStatusCode.Should().BeTrue();
+                registered = true;
+                return await regResult.FromJSONAsync<AuthSuccessResultDTO>();
+            });
+        }
+        public async Task<AuthSuccessResultDTO> RegisterAndLogIn()
+        {
+            var x = await RegisterAccount();
+            Client.DefaultRequestHeaders.Add("Bearer", x.Token);
+            return x;
+        }
+    }
     public class AuthIntegrationTests(IntegrationalFixture f) : IntegrationalTest(f)
     {
         private const string USER_PASSWORD = "123456789A123456789";
@@ -17,18 +54,67 @@ namespace IntegrationalTests
         public async Task CreateAccount()
         {
             string name = Faker.Internet.UserName();
-            var regResult = await Client.PostAsJsonAsync("/api/v1/auth/register", new AccountCreationDTO(name, USER_PASSWORD, null));
-            regResult.IsSuccessStatusCode.Should().BeTrue();
-
-
+            await RegisterAccount(name);
             var acc = Context.AccountsReadOnly.Where(x => x.Login == name).ToList();
             acc.Should().ContainSingle().Which.Login.Should().Be(name);
+            await RemoveAccount(name);
+        }
+        [AllureStep]
+        private async Task<AuthSuccessResultDTO> RegisterAccount(string name)
+        {
+            var regResult = await Client.PostAsJsonAsync("/api/v1/auth/register", new AccountCreationDTO(name, USER_PASSWORD, null));
+            regResult.IsSuccessStatusCode.Should().BeTrue();
+            return await regResult.FromJSONAsync<AuthSuccessResultDTO>();
+        }
+        [AllureStep]
+        private async Task RemoveAccount(string name)
+        {
+            var acc = Context.AccountsReadOnly.Where(x => x.Login == name).ToList();
             Context.AccountsReadOnly.RemoveRange(acc);
             await Context.SaveChangesAsync();
         }
-        protected override Task Init()
+        [Theory]
+        [InlineData("")]
+        [InlineData("thats my login!")]
+        [InlineData("****")]
+        public async Task CreateAccount_BadNickname(string name)
         {
-            return base.Init();
+            // Act
+            var regResult = await Client.PostAsJsonAsync("/api/v1/auth/register", new AccountCreationDTO(name, USER_PASSWORD, null));
+            // Assert
+            regResult.IsSuccessStatusCode.Should().BeFalse();
+            var acc = Context.AccountsReadOnly.Where(x => x.Login == name).ToList();
+            acc.Should().BeEmpty();
+        }
+        [Theory]
+        [InlineData("")]
+        [InlineData("123456")]
+        [InlineData("abcd")]
+        public async Task CreateAccount_BadPassword(string password)
+        {
+            // Arrange
+            string name = Faker.Internet.UserName();
+            // Act
+            var regResult = await Client.PostAsJsonAsync("/api/v1/auth/register", new AccountCreationDTO(name, password, null));
+            // Assert
+            regResult.IsSuccessStatusCode.Should().BeFalse();
+            var acc = Context.AccountsReadOnly.Where(x => x.Login == name).ToList();
+            acc.Should().BeEmpty();
+        }
+        [Fact]
+        public async Task LogIn()
+        {
+            // Arrange
+            string name = Faker.Internet.UserName();
+            await RegisterAccount(name);
+            // Act
+            var logResult = await Client.PostAsJsonAsync("/api/v1/auth/login", new AccountLoginDTO(name, USER_PASSWORD));
+            // Assert
+            var res = await logResult.FromJSONAsync<AuthSuccessResultDTO>();
+            var id = res.AccountID;
+            var acc = Context.AccountsReadOnly.Where(x => x.Id == id).ToList();
+            acc.Should().ContainSingle().Which.Login.Should().Be(name);
+            await RemoveAccount(name);
         }
     }
 }
---
+++ src/IntegrationalTests/IntegrationalTests/ParticipationTests.cs
@@ -0,0 +1,58 @@
+﻿using CompetitiveBackend;
+using CompetitiveBackend.BackendUsage.Objects;
+using CompetitiveBackend.Controllers;
+using CompetitiveBackend.Core.Auth;
+using CompetitiveBackend.Core.Objects;
+using CompetitiveBackend.Repositories;
+using CompetitiveBackend.Repositories.Exceptions;
+using FluentAssertions;
+using IntegrationalTests;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+namespace IntegrationalTests
+{
+    public class ParticipationTests(IntegrationalFixture f) : IntegrationalTest(f)
+    {
+        private RepositoriesRealisation.Models.CompetitionModel tracking;
+
+        [Fact]
+        public async Task Participate()
+        {
+            await using var REGISTERACC = new SetupAuth(Client, Context, Faker.Internet.UserName());
+            var ACCOUNT = await REGISTERACC.RegisterAndLogIn();
+            
+            // Act
+            var result = await Client.PutAsync($"/api/v1/competitions/{tracking.Id}/participations/?score=500", null);//await Client.GetAsync($"{APIConsts.ROOTV1}/{APIConsts.COMPETITIONS}/1");
+
+            // Assert
+            result.IsSuccessStatusCode.Should().BeTrue();
+            await Context.PlayerParticipation.Where(x => x.AccountID == ACCOUNT.AccountID).ExecuteDeleteAsync();
+        }
+        [Fact]
+        public async Task ParticipateNoLoggedIn()
+        {
+            // Act
+            var result = await Client.PutAsync($"/api/v1/competitions/{tracking.Id}/participations/?score=500", null);//await Client.GetAsync($"{APIConsts.ROOTV1}/{APIConsts.COMPETITIONS}/1");
+
+            // Assert
+            result.IsSuccessStatusCode.Should().BeFalse();
+            var x = Context.PlayerParticipation.Where(x => x.CompetitionID == tracking.Id).ToList();
+            x.Should().BeEmpty();
+        }
+        protected override async Task Init()
+        {
+            tracking = await Instantiate(new RepositoriesRealisation.Models.CompetitionModel("A", "B", DateTime.UtcNow, DateTime.UtcNow.AddDays(2)));
+            
+        }
+    }
+}
